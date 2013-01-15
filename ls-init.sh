64796c67e2999a2ef10817c0f9934ea3b2e5e0f4
Masen Marshall <mithofrose@gmail.com>
2013-01-15 17:57:11 -0600

    lshelp testing

--- ls-init.sh
+++ ls-init.sh
@@ -375,7 +375,7 @@ function lsap() {
 function lshelp2() {
 	for module in `ls -1 --color=never $LZS_MOD_PATH | cut -d . -f 1`
            do
-             echo -e "ls$module"\t\t"HelpMessage"
+             echo -e "ls$module \t\t HelpMessage"
 	  #   echo -e '-----------------'
 	  #   echo -e "Help Message\t"
 	     
